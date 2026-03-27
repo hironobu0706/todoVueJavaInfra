77dff9ae2547ef60596abfa68c73c2524acc72b0
DESKTOP-S9HU5A9\T <sample@yahoo.co.jp>
2026-03-27 15:50:19 +0900

    顧客情報テーブル作成を追加

--- initdb.d/02_create_table.sql
+++ initdb.d/02_create_table.sql
@@ -1,9 +1,20 @@
 -- 使用するDBを指定
 USE sampledb;
 
--- テーブル作成
-CREATE TABLE IF NOT EXISTS sample_table (
+-- 個人情報テーブル作成
+CREATE TABLE IF NOT EXISTS customer_info (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  mailAddress VARCHAR(255),
+  password VARCHAR(255),
+  name VARCHAR(255),
+  nameKana VARCHAR(255),
+  gender INT,
+  jushoPref INT,
+  jusho1 VARCHAR(255),
+  jusho2 VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  created_by VARCHAR(255),
+  update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  update_by VARCHAR(255),
+  delete_flg INT
 );
